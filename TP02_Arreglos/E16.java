e60328a1e9c6fb89eabb2406e719f5babf58de85
alezuuBytes <alexmine9999@gmail.com>
2026-04-05 18:39:09 -0300

    Primera parte TP arreglos entregable

--- TP02_Arreglos/E16.java
+++ TP02_Arreglos/E16.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class E16 {    
+public class E16 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int cant;
