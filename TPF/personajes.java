e2b19b23ddd260cbb31eda79ecf60b847e7f50c9
alezuuBytes <alexmine9999@gmail.com>
2026-06-10 00:17:56 -0300

    Trabajo Práctico Grupal Final - Keyla, Nacho, Alezuu

---
+++ TPF/personajes.java
@@ -0,0 +1,12 @@
+public class personajes {
+    private int codPersonaje;
+    private String nombrePersonaje;
+    private String tipoPersonaje;
+    private String energia;
+    private int duelosPerdidos;
+    private int duelosPerdidoss;
+
+    public personaje() {
+        
+    }
+}
